2e1f4d5a2c517852fc21d72380225c384a36bd09
orchita-roy <orchitaroy76@gmail.com>
2025-09-28 23:04:12 +0600

    initial

---
+++ main.js
@@ -0,0 +1,51 @@
+async function food() {
+
+  let response = await fetch(
+    `https://www.themealdb.com/api/json/v1/1/filter.php?a=${country}`
+  );
+  let obj = await response.json();
+  let foods = obj.meals;
+
+
+  container.innerHTML = "";
+
+  for (let food of foods) {
+    let div = document.createElement("div");
+    let h2 = document.createElement("h2");
+    let img = document.createElement("img");
+
+    div.className = "card bg-amber-200 shadow-sm ";
+    img.className = "image";
+    h2.className = "title";
+
+    if (food.strMealThumb) {
+      img.src = food.strMealThumb;
+    }
+    if (food.strMeal) {
+      h2.textContent = food.strMeal;
+    }
+
+    div.appendChild(img);
+    div.appendChild(h2);
+    container.appendChild(div);
+  }
+}
+
+let country = "";
+let container = document.querySelector(".container");
+let input = document.querySelector(".input");
+let button = document.querySelector(".btn");
+
+button.addEventListener("click", () => {
+  country = input.value.trim();
+  if (country) {
+    food();
+  }
+});
+
+document.addEventListener("keypress", (e) => {
+  country = input.value.trim();
+  if (e.key === "Enter" && country) {
+    food();
+  }
+});
